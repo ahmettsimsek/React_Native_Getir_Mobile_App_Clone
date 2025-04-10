ccfe3be9c41d3f5b7ed82ad23fb04f5652df9da8
Ahmtsmsek7 <ahmtsmsek7@gmail.com>
2025-04-10 15:11:06 +0300

    commit

---
+++ src/screens/CategoryFilterScreen/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text, ScrollView } from 'react-native'
+import React from 'react'
+
+const index = () => {
+  return (
+    <ScrollView>
+        <Text>Ahmet Şimşek</Text>
+    </ScrollView>
+  )
+}
+
+export default index
